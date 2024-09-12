22b846540a4dd54713f3ab49ec7b673fd535af1c
Andrei Lupascu <andrei.lupascu@hotmail.be>
2024-09-12 11:00:43 +0200

    a vast update of wonders and magic!

--- misc/init.py
+++ misc/init.py
@@ -1,27 +1,39 @@
-from config import config_me, default_config
+# from config import config_me, default_config
 
 
-def init_config():
+def init_config(mode="test"):
     """Initialize configuration. If user doesn't provide config, use default settings."""
 
-    user_config_completion = 0
-    missed_settings = []
+    CONFIG = {
+        "test": {
+            "mlflow_evals_nbr": 2,
+            "booster_rounds": 10
+        },
+        "prod": {
+            "mlflow_evals_nbr": 100,
+            "booster_rounds": 1000
+        }
+    }
+    # user_config_completion = 0
+    # missed_settings = []
 
-    for key, val in config_me.USER_CONFIG.items():
-        setting_length = len(val)
-        user_config_completion += setting_length
+    # for key, val in config_me.USER_CONFIG.items():
+    #     setting_length = len(val)
+    #     user_config_completion += setting_length
 
-        if setting_length == 0:
-            missed_settings.append(key)
+    #     if setting_length == 0:
+    #         missed_settings.append(key)
         
-    if user_config_completion == 0:
-        print("---User configuration not found. Loading default settings.")
-        return default_config.DEFAULT_CONFIG
-    elif len(missed_settings) > 0:
-        print(f"---Using default profile because the following settings are missing: {', '.join(missed_settings)}")
-        return default_config.DEFAULT_CONFIG
+    # if user_config_completion == 0:
+    #     print("---User configuration not found. Loading default settings.")
+    #     return default_config.DEFAULT_CONFIG
+    # elif len(missed_settings) > 0:
+    #     print(f"---Using default profile because the following settings are missing: {', '.join(missed_settings)}")
+    #     return default_config.DEFAULT_CONFIG
     
-    return config_me.USER_CONFIG
+    # return config_me.USER_CONFIG
+
+    return CONFIG[mode]
 
 
 if __name__ == "__main__":
--- misc/supports.py
+++ misc/supports.py
@@ -1,7 +1,66 @@
+import pandas as pd
+
+import xgboost as xgb
+from sklearn.metrics import roc_auc_score, log_loss, root_mean_squared_error
+
+from hyperopt import STATUS_OK
+
+import mlflow
+
+from typing import Any
 
 
-def get_params(test=True):
-    if test:
-        return 0
-    
-    return 1
+def get_objname(obj: Any) -> str:
+    """Retrieve the name of an object (for formating purposes)."""
+
+    obj_name =[v for v in globals() if globals()[v] is obj][0]    
+    return obj_name
+
+def objective(
+        search_space: dict,
+        xtrain: xgb.DMatrix,
+        xtest: xgb.DMatrix,
+        ytrain: pd.Series,
+        ytest: pd.Series,
+        num_boost_round: int
+        ) -> dict:
+    """Set-up the mlflow/hyperopt optimization process."""
+
+    metrics = []
+
+    with mlflow.start_run():
+        mlflow.log_params(search_space)
+        booster = xgb.train(
+            params=search_space,
+            dtrain=xtrain,
+            num_boost_round=num_boost_round,
+            evals=[(xtest, "test_set")],
+            early_stopping_rounds=50
+            )
+
+        # get predictions
+        ytrain_pred = booster.predict(xtrain)
+        yval_pred = booster.predict(xtest)
+        # get auc
+        train_auc = roc_auc_score(ytrain, ytrain_pred)
+        test_auc = roc_auc_score(ytest, yval_pred)
+        # get loss
+        train_log_loss = log_loss(ytrain, ytrain_pred)
+        test_log_loss = log_loss(ytest, yval_pred)
+        # get rmse
+        train_rmse = root_mean_squared_error(ytrain, ytrain_pred)
+        test_rmse = root_mean_squared_error(ytest, yval_pred)
+
+        # store metrics
+        metrics.extend([
+            train_auc, test_auc, 
+            train_log_loss, test_log_loss, 
+            train_rmse, test_rmse
+            ])
+        
+        # log metrics
+        for metric in metrics:
+            mlflow.log_metric(get_objname(metric), metric)
+
+        return {"loss": test_log_loss, "status": STATUS_OK}
+        
